6fef8aea75dd914e7007da2277b775b1afa2c696
Lisa Swart <arnoudlisa@gmail.com>
2021-01-19 18:08:07 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,9 +10,8 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  var thisIsCrazy = function (){
+  var thisIsCrazy = function() {
     console.log("hey!!!")
-
   thisIsCrazy();
   }
 }
